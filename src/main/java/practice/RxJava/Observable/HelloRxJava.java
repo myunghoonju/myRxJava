b8966ecd3a475cac06db5e6fc846d3eb7dbc4116
MYUNGHOON JU <myunghoonju@gmail.com>
2022-04-26 01:14:03 +0900

    Init RxJava
    Start!

---
+++ src/main/java/practice/RxJava/Observable/HelloRxJava.java
@@ -0,0 +1,13 @@
+package practice.RxJava.Observable;
+
+import io.reactivex.rxjava3.core.Observable;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HelloRxJava {
+
+    public static void main(String[] args) {
+        Observable<String> obs = Observable.just("HELLO", "RxJava");
+        obs.subscribe(data -> log.info("print data = {}", data));
+    }
+}
