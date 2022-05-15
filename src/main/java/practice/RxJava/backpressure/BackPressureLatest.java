57834eb1fe553646a92ba46847a85816dd156fcf
MYUNGHOON JU <myunghoonju@gmail.com>
2022-05-15 22:17:13 +0900

    Backpressure strategy sample

---
+++ src/main/java/practice/RxJava/backpressure/BackPressureLatest.java
@@ -0,0 +1,31 @@
+package practice.RxJava.backpressure;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import lombok.extern.slf4j.Slf4j;
+import practice.RxJava.MTimeUtil;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class BackPressureLatest {
+    /**
+     * 버퍼가 가득차면 버퍼 바깥쪽에서 통지 대기중인 데이터들은 계속 파기(DROP)하고
+     * 버퍼를 비운 시점에 Drop 되지 않고 대기중인 데이터부터 버퍼에 담는다.
+     */
+    public static void main(String[] args) {
+        Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .doOnNext(data -> log.info("#inverval doOnNext() {}", data))
+                .onBackpressureLatest()
+                .observeOn(Schedulers.computation(), false, 1)
+                .subscribe(
+                        data -> {
+                            MTimeUtil.sleep(1000L);
+                            log.info("process data :: onNext {}", data);
+                        },
+                        error -> log.info("onError {}", error)
+                );
+
+        MTimeUtil.sleep(5500L);
+    }
+}
