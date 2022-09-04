0d350147c057f063000501b236cc98192ea25f40
MYUNGHOON JU <myunghoonju@gmail.com>
2022-09-04 21:30:20 +0900

    Handling errors
    onError, onErrorReturn, onErrorRetry, onErrorResumeNext

---
+++ src/main/java/practice/RxJava/other/OnErrorReturnRxjava.java
@@ -0,0 +1,37 @@
+package practice.RxJava.other;
+
+import io.reactivex.rxjava3.core.Observable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class OnErrorReturnRxjava {
+
+    public static void main(String[] args) throws InterruptedException {
+        Observable
+                .just(5)
+                .flatMap(num -> Observable.interval(200L, TimeUnit.MILLISECONDS)
+                        .take(5)
+                        .map(i -> num / i)
+                        .onErrorReturn(ex -> {
+                            if (ex instanceof ArithmeticException) {
+                                log.info("error occurred {}", ex.getMessage());
+                            }
+                            return -1L;
+                        })
+                ).subscribe(
+                        data -> {
+                            if (data == -1) {
+                                log.info("error value returned");
+                            } else {
+                                log.info("success");
+                            }
+                        },
+                        error -> log.info("error occurred", error),
+                        () -> log.info("complete")
+                );
+
+        Thread.sleep(5000L);
+    }
+}
---
+++ src/main/java/practice/RxJava/other/OnErrorRxjava.java
@@ -0,0 +1,25 @@
+package practice.RxJava.other;
+
+import io.reactivex.rxjava3.core.Observable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class OnErrorRxjava {
+
+    public static void main(String[] args) throws InterruptedException {
+        Observable.just(5).flatMap(
+                num -> Observable.interval(200L, TimeUnit.MILLISECONDS)
+                        .doOnNext(data -> log.info("data = {}", data))
+                        .take(5)
+                        .map(i -> num / i)
+        ).subscribe(
+                data -> log.info("data = {}", data),
+                error -> log.info("error occurred", error),
+                () -> log.info("complete")
+        );
+
+        Thread.sleep(5000L);
+    }
+}
