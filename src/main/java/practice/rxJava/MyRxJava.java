30167a3a4a06000a28b4628fed626801c3231cd4
MYUNGHOON JU <myunghoonju@gmail.com>
2023-07-08 18:13:03 +0900

    tidy up

--- src/main/java/practice/rxJava/MyRxJava.java
+++ src/main/java/practice/rxJava/MyRxJava.java
@@ -5,7 +5,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
-public class MyRxJavaTest {
+public class MyRxJava {
 
     public static void main(String[] args) {
 /*        monoJust();
