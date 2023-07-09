b3a9ea33ae008a5a026aebb26a88842f5048cf33
MYUNGHOON JU <myunghoonju@gmail.com>
2023-07-10 01:09:36 +0900

    SO_LINGER
    no receive ack

--- src/main/java/practice/tcp/netty/sample/server/TestServer.java
+++ src/main/java/practice/tcp/netty/sample/server/TestServer.java
@@ -5,21 +5,25 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import practice.tcp.netty.sample.server.event.TestChannelHandler;
 
 public class TestServer {
 
     public void run() throws Exception {
-        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             System.out.println("SERVER RUNNING");
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
                     .childHandler(new TestChannelHandler())
                     //TODO option 확인
-                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .option(ChannelOption.SO_BACKLOG, 5)
+                    .option(ChannelOption.SO_LINGER, 0) // no wait time
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
 
             ChannelFuture sync = serverBootstrap.bind(8080).sync();
