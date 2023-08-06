71568a46af57ce5620fc251b8bbf46d0abaff5c3
MYUNGHOON JU <myunghoonju@gmail.com>
2023-08-06 17:21:06 +0900

    SENDING EMAIL TEST

--- src/main/java/practice/MyRxJavaApplication.java
+++ src/main/java/practice/MyRxJavaApplication.java
@@ -2,15 +2,12 @@ package practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import practice.tcp.netty.sample.server.TestServer;
 
 @SpringBootApplication
 public class MyRxJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRxJavaApplication.class, args);
-		TestServer testServer = new TestServer();
-		testServer.run();
 	}
 
 }
---
+++ src/main/java/practice/noti/EmailSender.java
@@ -0,0 +1,28 @@
+package practice.noti;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailSender {
+
+    public static final String[] MAILING_LIST = {"myunghoonju@naver.com"};
+    public static final String[] MAILING_CC_LIST = {"myunghoonju@naver.com"};
+
+    private final JavaMailSender javaMailSender;
+
+    public void mailSend() {
+        SimpleMailMessage textMsg = new SimpleMailMessage();
+        
+        textMsg.setFrom("myunghoonju@gmail.com");
+        textMsg.setTo(MAILING_LIST);
+        textMsg.setCc(MAILING_CC_LIST);
+        textMsg.setText("Hello \n\n Everyone! \n\n How's your day?");
+        textMsg.setSubject("Stopping by");
+
+        javaMailSender.send(textMsg);
+    }
+}
