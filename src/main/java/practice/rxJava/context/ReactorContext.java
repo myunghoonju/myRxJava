c5d47c96598b97dfa30a10f46667e0f9400cf044
MYUNGHOON JU <myunghoonju@gmail.com>
2024-01-23 01:23:40 +0900

    edit source

--- src/main/java/practice/rxJava/context/ReactorContext.java
+++ src/main/java/practice/rxJava/context/ReactorContext.java
@@ -1,6 +1,7 @@
 package practice.rxJava.context;
 
 import lombok.extern.slf4j.Slf4j;
+import practice.rxJava.utility.Log;
 import practice.rxJava.utility.Time;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -10,13 +11,13 @@ public class ReactorContext {
 
     public static void main(String[] args) {
         String key = "msg";
-        Mono<String> mono = Mono.deferContextual(ctx -> Mono.just("Hello " + ctx.get(key)).doOnNext(System.err::println))
+        Mono<String> mono = Mono.deferContextual(ctx -> Mono.just("Hello " + ctx.get(key)).doOnNext(Log::doOnNext))
                                 .subscribeOn(Schedulers.boundedElastic())
                                 .publishOn(Schedulers.parallel())
                                 .transformDeferredContextual((mono2, ctx) -> mono2.map(data -> data + " " + ctx.get(key)))
                                 .contextWrite(context -> context.put(key, "Reactor"));
 
-        mono.subscribe(System.err::println);
+        mono.subscribe(Log::doOnNext);
         Time.sleep(100L);
     }
 }
